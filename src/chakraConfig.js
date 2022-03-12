57bfdba03cbb7b47d3fce7b22279017cb66507ba
Chanel mepschen <c.mepschen@wearetriple.com>
2022-03-12 18:34:32 +0100

    feature: Add article cart

---
+++ src/chakraConfig.js
@@ -0,0 +1,14 @@
+const chakraConfig = {
+    colors: {
+        white: '#fff',
+        black: 'rgb(34, 34, 34)',
+        textGrey: 'rgb(102, 102, 102)',
+        red: 'rgb(230, 30, 20)',
+    },
+    fonts: {
+        body: '"Helvetica Neue", Helvetica, Arial, sans-serif',
+        heading: '"Helvetica Neue", Helvetica, Arial, sans-serif',
+    },
+};
+
+export default chakraConfig;
--- src/components/article-card.jsx
+++ src/components/article-card.jsx
@@ -1,12 +1,81 @@
-import { Heading, Box, Link, Image } from '@chakra-ui/react';
+import { Heading, Box, Link, Image, Text } from '@chakra-ui/react';
 
 export default function ArticleCard({ title, link, image, type }) {
     return (
-        <Box as="article" position="relative">
-            <Image src={image} objectFit="cover" />
-            <Box position="absolute">
-                <Heading as="h1">
-                    <Link href={link}>{title}</Link>
+        <Box
+            as="article"
+            position="relative"
+            role="group"
+            borderRadius="0.4rem"
+            overflow="hidden"
+        >
+            <Image
+                src={image}
+                objectFit="cover"
+                width="100%"
+                height="100%"
+                transition="transform 0.2s"
+                transform="scale(1)"
+                _groupHover={{ transform: 'scale(1.05)' }}
+            />
+            <Box
+                position="absolute"
+                bottom="0"
+                left="0"
+                right="0"
+                top="0"
+                display="flex"
+                flexDirection="column"
+                justifyContent="flex-end"
+                alignItems="flex-start"
+                padding={{ base: '25% 1em 1em 1em' }}
+                _before={{
+                    position: 'absolute',
+                    display: 'block',
+                    content: '""',
+                    left: 0,
+                    right: 0,
+                    bottom: 0,
+                    width: '100%',
+                    height: '100%',
+                    zIndex: 1,
+                    backgroundImage:
+                        'linear-gradient(rgba(0, 0, 0, 0) 25%, rgba(0, 0, 0, 0.7))',
+                }}
+            >
+                {(type === 'liveblog' || type === 'video') && (
+                    <Text
+                        display="inline-block"
+                        backgroundColor="red"
+                        color="white"
+                        fontSize="1.6rem"
+                        fontWeight="bold"
+                        zIndex="1"
+                        padding="0.5rem 1.2rem 0.5rem 2.5rem"
+                        marginBottom="1.2rem"
+                        lineHeight="1"
+                        borderRadius="2rem"
+                    >
+                        {type}
+                    </Text>
+                )}
+                <Heading as="h1" color="white" zIndex="1">
+                    <Link
+                        href={link}
+                        fontSize={{ base: '2.2rem', lg: '2.8rem' }}
+                        lineHeight={{ base: '2.4rem' }}
+                        _hover={{ textDecoration: 'none' }}
+                        _before={{
+                            position: 'absolute',
+                            content: '""',
+                            top: 0,
+                            left: 0,
+                            right: 0,
+                            bottom: 0,
+                        }}
+                    >
+                        {title}
+                    </Link>
                 </Heading>
             </Box>
         </Box>
